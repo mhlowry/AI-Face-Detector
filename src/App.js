623aa48a72a38cebc0790c558bbe523d4f31cd00
Michael Lowry <michael.lowry64@gmail.com>
2024-05-29 12:55:39 -0400

    Hooked up input and button to code

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import { useState } from 'react';
 
 function App() {
+  const [input, updateInput] = useState('');
+
+  const onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  const onSubmit = () => {
+    console.log('click');
+  }
+
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      <ImageLinkForm />
+      <ImageLinkForm onChange={onInputChange} onSubmit={onSubmit}/>
       {/* <FaceRecognition /> */}
       <Rank />
       
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,21 @@
 import React from "react";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = (props) => {
     return(
         <div>
             <p className="f3 b">
                 {'I can detect faces in your pictures, give it a try.'}
             </p>
             <div className="center ph5 w-3">
-                <input className="f4 pa2 w-70 center" type='text'></input>
-                <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
+                <input 
+                    onChange={props.onChange} 
+                    className="f4 pa2 w-70 center" type='text'>
+                </input>
+                <button 
+                    onClick={props.onSubmit} 
+                    className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+                    Detect
+                </button>
             </div>
         </div>
     );
