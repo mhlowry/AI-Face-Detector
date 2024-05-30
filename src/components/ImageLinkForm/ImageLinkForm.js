84fff3d13927905e5ca25e3e862b9cb3f94875ce
Michael Lowry <michael.lowry64@gmail.com>
2024-05-30 18:54:47 -0400

    Added enter key submit functionality

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,26 +1,43 @@
+import { useEffect } from "react";
 import React from "react";
 
 const ImageLinkForm = (props) => {
-    return(
-        <div>
-            <p className="f3 b">
-                {'I can detect faces in your pictures, give it a try.'}
-            </p>
-            <div className="center ph5 w-3">
-                <input 
-                    onChange={props.onChange} 
-                    className="f4 pa2 w-70 center" 
-                    type='text'
-                    placeholder="Image URL">
-                </input>
-                <button 
-                    onClick={props.onSubmit} 
-                    className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
-                    Detect
-                </button>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    const listener = event => {
+      if (event.code === "Enter" || event.code === "NumpadEnter") {
+        console.log("Enter key was pressed. Running the function.");
+        event.preventDefault();
+        props.onSubmit(); // Call the function
+      }
+    };
+
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, [props.onSubmit]);
+
+  return (
+    <div>
+      <p className="f3 b">
+        {'I can detect faces in your pictures, give it a try.'}
+      </p>
+      <div className="center ph5 w-3">
+        <input 
+          onChange={props.onChange} 
+          className="f4 pa2 w-70 center" 
+          type='text'
+          placeholder="Image URL"
+        />
+        <button 
+          onClick={props.onSubmit} 
+          className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+        >
+          Detect
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default ImageLinkForm;
