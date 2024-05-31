fe84133741e505c1a6f6a67d8ef3a1b7d31eb1c5
Michael Lowry <michael.lowry64@gmail.com>
2024-05-30 20:29:12 -0400

    Face detection boxes implemented

--- src/App.js
+++ src/App.js
@@ -1,119 +1,119 @@
+// App.js
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition'
-import { useState, useRef } from 'react';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import { useState } from 'react';
 
 function App() {
-  const [input, updateInput] = useState('');
-  const [imageURL, setImageURL] = useState('');
-  const [boxes, setBoxes] = useState([]);
-  const [aspectRatio, setAspectRatio] = useState(1);
-  const canvasRef = useRef(null);
-  const PAT = 'e4709956eb984194878fcd63cd8adbca';
-  const USER_ID = 'mhlowry';
-  const APP_ID = 'ai-face-detect';
-  const MODEL_ID = 'face-detection';
-    
-  async function getAspectRatio(imageUrl) {
-      return new Promise((resolve, reject) => {
-          const img = new Image();
-          img.src = imageUrl;
+    const [input, updateInput] = useState('');
+    const [imageURL, setImageURL] = useState('');
+    const [boxes, setBoxes] = useState([]);
+    const [aspectRatio, setAspectRatio] = useState(1);
+    const PAT = 'e4709956eb984194878fcd63cd8adbca';
+    const USER_ID = 'mhlowry';
+    const APP_ID = 'ai-face-detect';
+    const MODEL_ID = 'face-detection';
 
-          img.onload = () => {
-              const aspectRatio = img.width / img.height;
-              resolve(aspectRatio);
-          };
+    async function getAspectRatio(imageUrl) {
+        return new Promise((resolve, reject) => {
+            const img = new Image();
+            img.src = imageUrl;
 
-          img.onerror = (error) => {
-              reject(error);
-          };
-      });
-  }
-
-  const onInputChange = (event) => {
-     updateInput(event.target.value);
-  }
-
-  const onSubmit = ( event ) => {
-    getAspectRatio(input)
-          .then(aspectRatio => {
-            setAspectRatio(aspectRatio)
-            setImageURL(input);
-
-            // Create a JSON to send to the API
-            const raw = JSON.stringify({
-              "user_app_id": {
-                  "user_id": USER_ID,
-                  "app_id": APP_ID
-              },
-              "inputs": [
-                  {
-                      "data": {
-                          "image": {
-                              "url": input
-                              // "base64": IMAGE_BYTES_STRING
-                          }
-                      }
-                  }
-              ]
-            });
-
-            // Send request options to the API
-            const requestOptions = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Authorization': 'Key ' + PAT
-            },
-            body: raw
+            img.onload = () => {
+                const aspectRatio = img.width / img.height;
+                resolve(aspectRatio);
             };
 
+            img.onerror = (error) => {
+                reject(error);
+            };
+        });
+    }
 
-            fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
-              .then(response => response.json())
-              .then(result => {
+    const onInputChange = (event) => {
+        updateInput(event.target.value);
+    }
 
-                  const regions = result.outputs[0].data.regions;
+    const calculateFaceLocation = ({ left_col, top_row, right_col, bottom_row }) => {
+    const image = document.getElementById("img");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(`Image Width: ${width}, Image Height: ${height}`);
+    console.log(`Left Col: ${left_col}, Top Row: ${top_row}, Right Col: ${right_col}, Bottom Row: ${bottom_row}`);
+    return {
+        leftCol: left_col * width,
+        topRow: top_row * height,
+        rightCol: right_col * width,
+        bottomRow: bottom_row * height
+    }
+}
 
-                  regions.forEach(region => {
-                      // Accessing and rounding the bounding box values
-                      const boundingBox = region.region_info.bounding_box;
-                      const topRow = boundingBox.top_row.toFixed(3);
-                      const leftCol = boundingBox.left_col.toFixed(3);
-                      const bottomRow = boundingBox.bottom_row.toFixed(3);
-                      const rightCol = boundingBox.right_col.toFixed(3);
 
-                      region.data.concepts.forEach(concept => {
-                          // Accessing and rounding the concept value
-                          const name = concept.name;
-                          const value = concept.value.toFixed(4);
+    const onSubmit = (event) => {
+        getAspectRatio(input)
+            .then(aspectRatio => {
+                setAspectRatio(aspectRatio);
+                setImageURL(input);
 
-                          console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
-                          
-                      });
-                  });
-              })
-              .catch(error => console.log('error', error));
+                const raw = JSON.stringify({
+                    "user_app_id": {
+                        "user_id": USER_ID,
+                        "app_id": APP_ID
+                    },
+                    "inputs": [
+                        {
+                            "data": {
+                                "image": {
+                                    "url": input
+                                }
+                            }
+                        }
+                    ]
+                });
 
-                  })
-                  .catch(error => {
-                    console.error('Error loading image:', error)
-                  });            
-  }
+                const requestOptions = {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Authorization': 'Key ' + PAT
+                    },
+                    body: raw
+                };
 
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm onChange={onInputChange} onSubmit={onSubmit}/>
-      <FaceRecognition imageURL={imageURL} aspectRatio={aspectRatio}/>
-      <Rank />
-      
-    </div>
-  );
+                fetch(`https://api.clarifai.com/v2/models/${MODEL_ID}/outputs`, requestOptions)
+                    .then(response => response.json())
+                    .then(result => {
+                        const regions = result.outputs[0].data.regions;
+                        const newBoxes = regions.map(region => {
+                            const boundingBox = region.region_info.bounding_box;
+                            return calculateFaceLocation({
+                                left_col: boundingBox.left_col,
+                                top_row: boundingBox.top_row,
+                                right_col: boundingBox.right_col,
+                                bottom_row: boundingBox.bottom_row
+                            });
+                        });
+                        setBoxes(newBoxes);
+                    })
+                    .catch(error => console.log('error', error));
+            })
+            .catch(error => {
+                console.error('Error loading image:', error)
+            });
+    }
+
+    return (
+        <div className="App">
+            <Navigation />
+            <Logo />
+            <ImageLinkForm onChange={onInputChange} onSubmit={onSubmit} />
+            <FaceRecognition boxes={boxes} imageURL={imageURL} aspectRatio={aspectRatio} />
+            <Rank />
+        </div>
+    );
 }
 
 export default App;
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,17 +1,48 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageURL, aspectRatio }) => {
-        const DEFAULT_HEIGHT = 512;
+const FaceRecognition = ({ boxes, imageURL, aspectRatio }) => {
+    const DEFAULT_HEIGHT = 512;
+    const canvasRef = useRef(null);
 
-        return (
-            <div className="outsideWrapper center" style={{width:`${DEFAULT_HEIGHT*aspectRatio}px`}}>
-                <div className="insideWrapper">
-                    <img src={imageURL} className="coveredImage" alt=''></img>
-                    <canvas className="coveringCanvas"></canvas>
-                </div>
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+        const image = new Image();
+        image.src = imageURL;
+
+        image.onload = () => {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+
+            // Set canvas dimensions based on aspect ratio and parent container width
+            const canvasWidth = canvas.parentElement.offsetWidth;
+            const canvasHeight = canvasWidth / aspectRatio;
+            canvas.width = canvasWidth;
+            canvas.height = canvasHeight;
+
+            // Draw the image on the canvas
+            context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+            // Draw each box
+            boxes.forEach(box => {
+                const { leftCol, topRow, rightCol, bottomRow } = box;
+                context.beginPath();
+                context.lineWidth = '2';
+                context.strokeStyle = 'red';
+                context.rect(leftCol, topRow, rightCol - leftCol, bottomRow - topRow);
+                context.stroke();
+            });
+        };
+    }, [imageURL, boxes, aspectRatio]);
+
+    return (
+        <div className="outsideWrapper center" style={{ width: `${DEFAULT_HEIGHT * aspectRatio}px` }}>
+            <div className="insideWrapper">
+                <img id="img" src={imageURL} className="coveredImage" alt='' />
+                <canvas ref={canvasRef} className="coveringCanvas"></canvas>
             </div>
-        );
-    }
+        </div>
+    );
+}
 
 export default FaceRecognition;
