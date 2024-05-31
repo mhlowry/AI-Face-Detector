5b21e93262274c5cc5389d43a08cd6592551b220
Michael Lowry <michael.lowry64@gmail.com>
2024-05-31 15:30:31 -0400

    Changed form to div
    And a small change on register page, Name -> First Name.

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -4,11 +4,11 @@ const Register = ({ onRouteChange }) => {
     return (
         <article className="br3 shadow-5 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
             <main className="pa4 black-80">
-                <form className="measure">
+                <div className="measure">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                     <legend className="f2 fw6 ph0 mh0">Register</legend>
                     <div className="mt3">
-                        <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+                        <label className="db fw6 lh-copy f6" htmlFor="name">First Name</label>
                         <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
                     </div>
                     <div className="mt3">
@@ -27,7 +27,7 @@ const Register = ({ onRouteChange }) => {
                             type="submit" 
                             value="Register" />
                     </div>
-                </form>
+                </div>
             </main>
         </article>
     );
